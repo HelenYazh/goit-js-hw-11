c379be1525e90d0574a403f8d43cf2bef580753d
HelenYazh <olenka.yazh05@gmail.com>
2024-04-16 21:39:05 +0300

    task done

--- src/main.js
+++ src/main.js
@@ -3,14 +3,19 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { fetchImages } from "../src/js/pixabay-api.js";
-import { clearGallery, createMarkup } from "../src/js/render-functions.js";
+import { createMarkup, clearGallery } from "../src/js/render-functions.js";
 
 
 const form = document.querySelector(".search-form");
 const input = document.querySelector("#search-input");
-const list = document.querySelector(".gallery");
+const loader = document.querySelector('.loader');
 
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
+loader.style.display = "none";
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
@@ -19,33 +24,22 @@ function handleSubmit(event) {
     if (input.value.trim() === "") {
         return;
     }
+    loader.style.display = "block";
+    clearGallery();
 
-
-    fetch(`https://pixabay.com/api?${params}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status)
-            }
-
-            return response.json();
-        })
-        .then(data => {
-            if (data.hits.length === 0) {
+    fetchImages(input.value.trim())
+        .then(img => {
+            if (img.length === 0) {
                 iziToast.error({
                     message: "Sorry, there are no images matching your search query. Please try again!",
                     position: "topRight",
                 })
-            }
-            list.insertAdjacentHTML("beforeend", createMarkup(data.hits));
+            };
+            createMarkup(img);
+            lightbox.refresh();
         })
         .catch(error => console.log(error))
+        .finally(() => loader.style.display = "none")
 
-}
 
-function createMarkup(arr) {
-    return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
-        `<li>
-          <img src="${webformatURL}" alt="${tags}"
-        </li>`)
-        .join("")
 }
